19adb18e268e084c44f284724f4bd11a34218c7c
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-09 02:08:29 +0600

    error fixed

--- m-67/simple-CRUD/index.js
+++ m-67/simple-CRUD/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // mongo db
@@ -13,38 +14,37 @@ app.use(express.json());
 // X5dUy2DhWOfeL9W6
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://delwerhossain006:X5dUy2DhWOfeL9W6@simple-del.4ijtj0g.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://delwerhossain006:X5dUy2DhWOfeL9W6@simple-del.4ijtj0g.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 // port 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    app.send('simple CRUD')
+    res.send('simple CRUD')
 })
 
 app.listen(port, () => {
