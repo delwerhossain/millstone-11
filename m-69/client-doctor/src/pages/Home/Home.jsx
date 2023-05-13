082b4998210b2fffb610654a345df0dcf2c111e2
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-13 10:01:26 +0600

    service and about part added

--- m-69/client-doctor/src/pages/Home/Home.jsx
+++ m-69/client-doctor/src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import HomeBanner from '../../Components/HomeBanner/HomeBanner';
+import HomeAbout from '../../Components/HomeAbout/HomeAbout';
+import HomeServices from '../../Components/HomeServices/HomeServices';
 
 const Home = () => {
     return (
@@ -7,6 +9,8 @@ const Home = () => {
             <h1 className='p-4 text-white bg-indigo-500 rounded-lg'>Home</h1>
 
             <HomeBanner></HomeBanner>
+            <HomeAbout></HomeAbout>
+            <HomeServices></HomeServices>
         </div>
     );
 };
