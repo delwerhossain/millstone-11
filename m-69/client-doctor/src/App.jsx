272c1352e4dcf9665cc06ffea477c444b735cba8
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-14 16:15:06 +0600

    fix error

--- m-69/client-doctor/src/App.jsx
+++ m-69/client-doctor/src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
