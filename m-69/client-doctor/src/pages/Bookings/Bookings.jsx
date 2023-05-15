787b5ce1c168644f0ded582f5f234b4cd1d06185
delwerhossain <delwerhossain006@gmail.com>
2023-05-15 16:05:10 +0100

    error handle error

--- m-69/client-doctor/src/pages/Bookings/Bookings.jsx
+++ m-69/client-doctor/src/pages/Bookings/Bookings.jsx
@@ -1,10 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
 import BookingRow from "./BookingRow";
+import { useNavigate } from "react-router-dom";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
   const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
   const url = `http://localhost:5000/bookings?email=${user?.email}`;
   useEffect(() => {
     fetch(url, {
@@ -14,8 +16,14 @@ const Bookings = () => {
       },
     })
       .then((res) => res.json())
-      .then((data) => setBookings(data));
-  }, []);
+      .then((data) => {
+        if (!data.error) {
+          setBookings(data);
+        } else {
+          navigate("/");
+        }
+      });
+  }, [url, navigate]);
 
   // delete bookings
   const handleDelete = (_id) => {
--- m-69/server-doctor/index.js
+++ m-69/server-doctor/index.js
@@ -61,7 +61,7 @@ async function run() {
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: 10,
+        expiresIn: 30,
       });
       res.send({ token });
     });
