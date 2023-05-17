1cc893bfb213e16be899e125fc711673169803e7
Delwer Hossain <delwerhossain006@gmail.com>
2023-05-17 06:03:01 +0600

    mongo connect

--- m-72/server-ema-jhon/index.js
+++ m-72/server-ema-jhon/index.js
@@ -2,10 +2,42 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+require("dotenv").config();
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//mongodb connection
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@simple-del.4ijtj0g.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 //api
 app.get("/", async (req, res) => {
